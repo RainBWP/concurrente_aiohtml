e1db8a4b8a0bbc81c9ff3a92b757c20e8d0e5170
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-06 01:20:48 -0600

    Agregando backend para convertir imagenes

--- server.py
+++ server.py
@@ -1,25 +1,41 @@
 from aiohttp import web
+from PIL import Image
 import os
 
 # Ruta del directorio estático
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
+# Configuración para GitHub Pages
 
 # Manejador para la página principal
 async def handle_index(request):
     return web.FileResponse(os.path.join(static_dir, 'index.html'))
 
-# Manejador para una ruta de backend
+# Manejador para la API
+# Se espera que la imagen sea enviada por POST
+# http://[host]/convert
 async def handle_api(request):
-    data = {'message': 'Hola desde el backend'}
-    return web.json_response(data)
+    # Obtener la imagen enviada por POST
+    data = await request.post()
+    image = data['image'].file.read()
+    # Crear un objeto Image
+    img = Image.open(io.BytesIO(image))
+    # Convertir la imagen a escala de grises
+    img = img.convert('L')
+    # Guardar la imagen en un buffer
+    buffer = io.BytesIO()
+    img.save(buffer, format='PNG')
+    # Devolver la imagen convertida
+    return web.Response(body=buffer.getvalue(), content_type='image/png')
 
 # Crear la aplicación y configurar las rutas
 app = web.Application()
 app.add_routes([
     web.get('/', handle_index),
-    web.get('/api', handle_api),
+    web.post('/convert', handle_api),
     web.static('/static', static_dir)  # Servir archivos estáticos
 ])
 
 if __name__ == '__main__':
-    web.run_app(app)
+    host = os.getenv('HOST', '0.0.0.0') # obtener dirección del host
+    port = int(os.getenv('PORT', 8080)) # obtener puerto del host
+    web.run_app(app, host=host, port=port) # iniciar la aplicación
