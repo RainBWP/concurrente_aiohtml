eb46877e8282518f87d214426b6f61bc664101ef
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-06 02:02:10 -0600

    Añadir soporte para conversión de imágenes y configuración de CORS en el servidor

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+import io
 from aiohttp import web
 from PIL import Image
 import os
+import aiohttp_cors
 
 # Ruta del directorio estático
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
-# Configuración para GitHub Pages
 
 # Manejador para la página principal
 async def handle_index(request):
@@ -14,18 +15,33 @@ async def handle_index(request):
 # Se espera que la imagen sea enviada por POST
 # http://[host]/convert
 async def handle_api(request):
-    # Obtener la imagen enviada por POST
+    # Obtener la imagen y el formato enviados por POST
     data = await request.post()
     image = data['image'].file.read()
+    format = data['format']
+    quality = 100
+
+    if format == 'meme':
+        format = 'jpeg'
+        quality = 1
+
     # Crear un objeto Image
     img = Image.open(io.BytesIO(image))
-    # Convertir la imagen a escala de grises
-    img = img.convert('L')
-    # Guardar la imagen en un buffer
+    
+    # Convertir la imagen al formato deseado
     buffer = io.BytesIO()
-    img.save(buffer, format='PNG')
+    img.save(buffer, format=format.upper(), quality=quality)
+    buffer.seek(0)
+    
+    # Guardar la imagen convertida en el directorio estático
+    output_filename = f'converted_image.{format}'
+    output_path = os.path.join(static_dir, output_filename)
+    with open(output_path, 'wb') as f:
+        f.write(buffer.getvalue())
+    
     # Devolver la imagen convertida
-    return web.Response(body=buffer.getvalue(), content_type='image/png')
+    response_data = {'filename': output_filename}
+    return web.json_response(response_data)
 
 # Crear la aplicación y configurar las rutas
 app = web.Application()
@@ -35,7 +51,21 @@ app.add_routes([
     web.static('/static', static_dir)  # Servir archivos estáticos
 ])
 
+# Configurar CORS
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*",
+    )
+})
+
+# Añadir CORS a las rutas
+for route in list(app.router.routes()):
+    cors.add(route)
+
 if __name__ == '__main__':
-    host = os.getenv('HOST', '0.0.0.0') # obtener dirección del host
-    port = int(os.getenv('PORT', 8080)) # obtener puerto del host
-    web.run_app(app, host=host, port=port) # iniciar la aplicación
+    host = os.getenv('HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', 8080))
+    print(f"Starting server on {host}:{port}")
+    web.run_app(app, host=host, port=port)
