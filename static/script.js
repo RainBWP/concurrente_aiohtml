8a2f5b1109cff9b2e6f53c0ef46e36bea42d2b52
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-06 00:48:01 -0600

    showImage ahora guardara la imagen globalmente

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+var global_image = new FormData();
+
 function call_https_request(from) {
     var url = 'http://0.0.0.0:8080/';
     var xhr = new XMLHttpRequest();
@@ -14,9 +16,21 @@ function call_https_request(from) {
 function showImage(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
+    var fileformat;
     reader.onload = function(e) {
         document.getElementById('preview').src = e.target.result;
         document.getElementById('preview').hidden = false;
+        fileformat = file.name.split('.').pop().toLowerCase();
+
+        // hide label that is the same format as the image
+        var labelFormats = document.getElementById('selectorformat').getElementsByTagName('label');
+        for (var i = 0; i < labelFormats.length; i++) {
+            if (labelFormats[i].value == fileformat) {
+                labelFormats[i].disabled = true;
+                labelFormats[i].checked = false;
+            }
+        }
+        global_image.append('image', file);
     };
     reader.readAsDataURL(file);
 }
