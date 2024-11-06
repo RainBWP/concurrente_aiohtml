37c87c4497ff4e7d8fed806a5119ab941fc76adf
RainBWP <114127654+RainBWP@users.noreply.github.com>
2024-11-06 01:21:35 -0600

    Mejora en la gestión de imágenes: se agrega función para enviar imágenes al servidor y se actualiza la variable global para almacenar datos de imagen.

--- static/script.js
+++ static/script.js
@@ -1,7 +1,10 @@
-var global_image = new FormData();
+var global_image = new FormData(); // global variable to store image data
 
-function call_https_request(from) {
-    var url = 'http://0.0.0.0:8080/';
+console.log(window.location.origin)
+
+
+function call_https_request(from) { // basic function to call the server
+    var url = window.location.origin;
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
@@ -13,7 +16,7 @@ function call_https_request(from) {
     xhr.send();
 }
 
-function showImage(event) {
+function showImage(event) { // show image preview
     const file = event.target.files[0];
     const reader = new FileReader();
     var fileformat;
@@ -23,14 +26,32 @@ function showImage(event) {
         fileformat = file.name.split('.').pop().toLowerCase();
 
         // hide label that is the same format as the image
-        var labelFormats = document.getElementById('selectorformat').getElementsByTagName('label');
+        var labelFormats = document.getElementsByClassName('selectorformat');
+        
         for (var i = 0; i < labelFormats.length; i++) {
-            if (labelFormats[i].value == fileformat) {
+            if (labelFormats[i].value === fileformat) {
                 labelFormats[i].disabled = true;
                 labelFormats[i].checked = false;
             }
         }
         global_image.append('image', file);
+        document.getElementById('convertButton').disabled = false;
     };
     reader.readAsDataURL(file);
+}
+
+function sendImage() { // send image to server
+    var url = window.location.origin + '/convert'; // server url
+    var xhr = new XMLHttpRequest(); 
+    xhr.open('POST', url, true);
+    xhr.send(global_image);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById('resultado').hidden = false;
+            var data = JSON.parse(xhr.responseText);
+            console.log(data);
+            document.getElementById('download').hidden = false;
+            document.getElementById('download').href = 'static/' + data['filename'];
+        }
+    }
 }
